7529b74c1f0797bcea941c58a08cd54af49f8570
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-15 16:57:18 -0300

    feat: exige SSL na configuração do banco de dados

--- config/database.ts
+++ config/database.ts
@@ -42,6 +42,7 @@ const databaseConfig: DatabaseConfig = {
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
         ssl: {
+          require: true,
           rejectUnauthorized: false,
         },
       }),
