d92e46131d460134c2325812ae2a27353b5ae5b5
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-18 14:43:30 -0300

    feat: remove configuração SSL do banco de dados

--- config/database.ts
+++ config/database.ts
@@ -41,7 +41,6 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: true,
       }),
       migrations: {
         naturalSort: true,
