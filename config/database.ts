964f65a5afb7dfcfa2015b20a2fa7cdc2c148e5a
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-18 14:26:48 -0300

    feat: adiciona configuração requestCert para SSL no banco de dados

--- config/database.ts
+++ config/database.ts
@@ -43,6 +43,7 @@ const databaseConfig: DatabaseConfig = {
         database: Env.get('PG_DB_NAME'),
         ssl: {
           rejectUnauthorized: false,
+          requestCert: true,
         },
       }),
       migrations: {
