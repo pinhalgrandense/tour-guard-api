3fa5fe44f73ff6828e94865dbb5abbf5e1c62993
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-18 15:02:03 -0300

    feat: adiciona suporte a HTTPS com configuração de certificados no servidor

--- server.ts
+++ server.ts
@@ -13,9 +13,18 @@
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
 import { Ignitor } from '@adonisjs/core/build/standalone'
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
+const privateKey = readFileSync(join(__dirname + '/build/sslCert/server.key'), 'utf8');
+const certificate = readFileSync(join(__dirname + '/build/sslCert/server.crt'), 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
 new Ignitor(__dirname)
   .httpServer()
-  .start()
+  .start((handle) => {
+    return createServer(credentials, handle);
+  })
