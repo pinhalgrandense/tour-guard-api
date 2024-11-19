4bafa6bec93605645fb3750917f997aeabddeef2
Gregori Rubin <gregori.rubin@gmail.com>
2024-11-19 17:33:07 -0300

    feat: atualiza configuração de SSL para permitir conexões não autorizadas

--- config/database.ts
+++ config/database.ts
@@ -41,7 +41,9 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get("PG_USER"),
         password: Env.get("PG_PASSWORD", ""),
         database: Env.get("PG_DB_NAME"),
-        ssl: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }),
       migrations: {
         naturalSort: true,
